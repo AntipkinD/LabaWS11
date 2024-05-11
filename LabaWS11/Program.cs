1573b927beefaa94d39271ced25b9046e2e2a8f2
mushroomvoodoo <normeeboy@gmail.com>
2024-05-11 12:00:24 +0300

    ...

--- LabaWS11/Program.cs
+++ LabaWS11/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿internal class Program
+{
+    delegate string WeekText();
+    private static void Main(string[] args)
+    {
+        string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        int i = 0;
+        WeekText first = () => { string day = days[i]; i++; return day; };
+        for ( ; ; )
+        {
+            if (i > 6) i = 0;
+            Console.ReadLine();
+            Console.WriteLine(first?.Invoke());
+        }
+    }
+}
